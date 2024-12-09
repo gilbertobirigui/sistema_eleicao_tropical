64b85364a3f1f81d0a5dc0778655fb333f36365a
gilbertoBirigui <gfilmesbiriguihotmail.com>
2024-12-09 12:45:16 -0300

    template base (heranca de template)

--- core/urls.py
+++ core/urls.py
@@ -21,30 +21,9 @@ from django.conf.urls.static import static
 from django.urls import path
 
 
-import logging
-from django.http import JsonResponse
-
-
-# Obtenha o logger configurado
-logger = logging.getLogger('requestlogs')
-
-
-
-def test_logging_view(request):
-    try:
-        data = {'user': 'gilberto', 'email': 'gilberto@contato.com'}
-        # Força um erro proposital para testar o log
-        raise ValueError("Erro simulado no envio de email")
-    except Exception as e:
-        # Loga o erro usando o logger configurado
-        logger.error(f"{str(e)} | {str(data)}")
-        return JsonResponse({"status": "error", "message": "Ocorreu um erro."})
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test-logging/', test_logging_view, name='test_logging'),
-    
+       
    ]
 
 
